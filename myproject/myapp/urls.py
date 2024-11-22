d7ecc0abdf10caae51f2557a06917c7c71f557db
SalTheSWE <123565018+SalTheSWE@users.noreply.github.com>
2024-11-22 23:56:18 +0300

    new

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,6 +3,22 @@ from rest_framework.routers import DefaultRouter
 from .views import login_user, register_user, TransactionViewSet, AccountViewSet, BudgetGoalViewSet, SavingsGoalViewSet
 from .views import ChatSessionView, SendMessageView, ChatHistoryView, AIResponseView
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="API endpoints and usage",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+
 
 # Setting up the router for the viewsets
 router = DefaultRouter()
@@ -25,4 +41,8 @@ urlpatterns = [
     # Register and login endpoints
     path('api/register/', register_user, name='register_user'),
     path('api/login/', login_user, name='login_user'),
+]
+urlpatterns += [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   # path('swagger.yaml', schema_view.without_ui(cache_timeout=0, renderer_classes=[ 'drf_yasg.renderers.OpenAPIRenderer',]), name='schema-yaml'),
 ]
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "channels",
-    "django_extensions"
+    "django_extensions",
+    "drf_yasg"
  
 ]
 
